9cf699da5a5e6fd45ddf8ea8e6fbc724b4f5d882
Aurélien Brabant <main@aurelienbrabant.fr>
2023-09-11 22:09:47 +0200

    create first tables

--- server/migrations/1694446854626_initial.js
+++ server/migrations/1694446854626_initial.js
@@ -2,6 +2,49 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ * */
+exports.up = (pgm) => {
+	pgm.db.query(`
+		CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-exports.down = pgm => {};
+		CREATE TABLE IF NOT EXISTS users(
+			id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+			username VARCHAR(255) NOT NULL,
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			sign_in_method VARCHAR(30) NOT NULL DEFAULT 'email-password'
+		);
+		CREATE TABLE IF NOT EXISTS conversations(
+			id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			agent_id VARCHAR(255) NOT NULL,
+			user_id uuid NOT NULL REFERENCES users(id)
+		);
+		CREATE TABLE IF NOT EXISTS tasks (
+			id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+			content TEXT NOT NULL,
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			conversation_id uuid NOT NULL REFERENCES conversations(id)
+		);
+		CREATE TABLE IF NOT EXISTS messages (
+			id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+			content TEXT NOT NULL,
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			task_id uuid NOT NULL REFERENCES tasks(id),
+			type VARCHAR(30) NOT NULL
+		);
+	`);
+};
+
+exports.down = pgm => {
+	pgm.db.query(`
+		DROP TABLE IF EXISTS messages;
+		DROP TABLE IF EXISTS tasks;
+		DROP TABLE IF EXISTS conversations;
+		DROP TABLE IF EXISTS users;
+	`);
+};
