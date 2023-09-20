491b06c874efa0a8827ae9a4b33d0cbce6a8daec
Kevin Piacentini <kevinpiac@gmail.com>
2023-09-20 18:57:31 +0200

    Remove svelte twitter emoji

--- frontend/src/lib/components/chat/bubble/Bubble.svelte
+++ frontend/src/lib/components/chat/bubble/Bubble.svelte
@@ -3,24 +3,22 @@
 	import type { ComponentType } from "svelte";
 	import type { BubbleType } from "$lib/components/chat/bubble/bubble.types";
 
-	import { A1F9e0, A1F4a1, A1F916 } from "svelte-twitter-emoji";
-
 	export let title: string;
 	export let time: string;
 	export let body: string;
 	export let from: "user" | "agent";
 
-	export let type: BubbleType = 'info';
+	export let type: BubbleType = "info";
 
 	let bubbleClass: string;
 
 	$: bubbleClass = from === "user" ? "bg-bubble-bg-primary" : "bg-bubble-bg-secondary";
 
-	const typeToEmoji: Record<BubbleType, ComponentType> = {
-		thinking: A1F9e0,
-		action: A1F916,
-		info: A1F4a1,
-		error: A1F4a1,
+	const typeToEmoji: Record<BubbleType, string> = {
+		thinking: "thinking",
+		action: "action",
+		info: "info",
+		error: "error"
 	};
 </script>
 
@@ -32,7 +30,7 @@
 					alt="user avatar"
 					src="https://media.licdn.com/dms/image/D4E03AQFXJiFpNFWE0A/profile-displayphoto-shrink_100_100/0/1680893451739?e=1699488000&v=beta&t=WiNliB67TjMHbaIycm8u55JDrX82xu9I20jw-b10u4A" />
 			{:else if from === "agent"}
-				<svelte:component this={typeToEmoji[type]} class="w-5" />
+				<span>{typeToEmoji[type]} }</span>
 			{/if}
 
 			<div>
