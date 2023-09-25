d12bb6df652e10425822248e0c86a48cd4f7d923
Kevin Piacentini <kevinpiac@gmail.com>
2023-09-25 14:14:55 +0200

    wip

--- console/src/routes/onboarding/project/[projectId]/ready/+page.svelte
+++ console/src/routes/onboarding/project/[projectId]/ready/+page.svelte
@@ -5,9 +5,13 @@
 	import { Icon, CheckCircle, CursorArrowRays } from "svelte-hero-icons";
 	import Button from "$lib/components/common/button/Button.svelte";
 	import Spacer from "$lib/components/common/spacer/Spacer.svelte";
+	import Monaco from "svelte-monaco";
+	import { onboardingPythonCode } from "$lib/components/project/agents/code-snippets/onboarding.snippet";
 
 	let currentStep: "open-frontend" | "authentication" | "send-message" = "open-frontend";
 	$: currentStepIndex = ["open-frontend", "authentication", "send-message"].indexOf(currentStep);
+
+	let pythonCodeSnippet = onboardingPythonCode;
 
 	let user: {
 		name: string;
@@ -25,8 +29,8 @@
 		};
 	};
 
-	const handleNewMessage = () => {
-		messages = [...messages, "Hello World!"];
+	const completeOnboarding = () => {
+		// not implemented
 	};
 </script>
 
@@ -46,20 +50,18 @@
 		</section>
 	</TopCover>
 	<div class="w-full">
-		<div class="max-w-6xl m-auto mt-[-30px]">
+		<div class="max-w-7xl m-auto mt-[-30px]">
 			<Card>
-				<section class="p-10 antialiased min-h-[200px] grid grid-cols-2 gap-3">
-					<div>
+				<section class="p-10 antialiased min-h-[200px] grid grid-cols-6 gap-3">
+					<div class="col-span-2">
 						<div class={currentStep === "open-frontend" ? "" : "disabled opacity-20"}>
 							<Typography type="label">Step 1</Typography>
-							<Typography type="sectionTitle">Open your frontend</Typography>
+							<Typography type="sectionTitle">Test your Agent UI</Typography>
 							<Spacer size="sm" />
-							<div class="grid grid-cols-2 gap-3">
-								<Button
-									on:click={() => (currentStep = "authentication")}
-									size="bigger"
-									leftIcon={CursorArrowRays}>Open my frontend</Button>
-							</div>
+							<Button
+								on:click={() => (currentStep = "authentication")}
+								size="bigger"
+								leftIcon={CursorArrowRays}>Open the demo Agent</Button>
 							<Spacer size="md" />
 						</div>
 						<div class={currentStep === "authentication" ? "" : "disabled opacity-20"}>
@@ -77,67 +79,40 @@
 						</div>
 						<div class={currentStep === "send-message" ? "" : "disabled opacity-20"}>
 							<Typography type="label">Step 3</Typography>
-							<Typography type="sectionTitle">Send a message in the Chat</Typography>
+							<Typography type="sectionTitle">Implement your own logic</Typography>
 							<Spacer size="sm" />
 							<div class="grid grid-cols-2 gap-3">
 								<Button
-									on:click={handleNewMessage}
+									on:click={completeOnboarding}
 									size="bigger"
-									leftIcon={CursorArrowRays}>Continue</Button>
+									leftIcon={CursorArrowRays}>Let's go!</Button>
 							</div>
 							<Spacer size="md" />
 						</div>
 					</div>
-					<div class="flex items-center justify-center">
+					<div class="flex items-center justify-center col-span-4">
 						<div
 							class="bg-background-quaternary dark:bg-background-quaternary-dark rounded-md py-4 px-4 flex flex-col items-center justify-center w-full h-full gap-3 relative">
-							<div class="w-full flex flex-col gap-3">
-								{#if !!user}
-									<div
-										class="animate-in duration-700 fade-in slide-in slide-in-from-bottom-20">
-										<Card>
-											<div class="p-4 flex gap-3">
-												<span
-													class="text-body-accent dark:text-body-accent-dark"
-													>New user created!
-												</span>
-												<span
-													class="text-body-base dark:text-body-dark font-semibold"
-													>{user.name}</span>
-												<span class="text-body-base dark:text-body-dark"
-													>{user.email}</span>
-											</div>
-										</Card>
-									</div>
-								{/if}
-								{#each messages as message}
-									<div
-										class="animate-in duration-700 fade-in slide-in slide-in-from-bottom-20">
-										<Card>
-											<div class="p-4 flex gap-3">
-												<span
-													class="text-body-accent dark:text-body-accent-dark"
-													>New frontend message!
-												</span>
-												<span
-													class="text-body-base dark:text-body-dark font-semibold overflow-ellipsis"
-													>{message}</span>
-											</div>
-										</Card>
-									</div>
-								{/each}
-							</div>
-							<div
-								class="{currentStepIndex < 2
-									? ''
-									: 'absolute bottom-5 right-5'} rounded-md bg-white p-3 flex items-center justify-center gap-4 text-sm text-body-base">
-								<span class="relative flex h-3 w-3">
-									<span
-										class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75" />
-									<span
-										class="relative inline-flex rounded-full h-3 w-3 bg-sky-500" />
-								</span>
-								Waiting for events...
+							<div class="w-full h-full rounded-md overflow-hidden">
+								<div id="editor" class="h-full">
+									<!-- event.detail is the monaco instance. All options are reactive! -->
+									<Monaco
+										options={{
+											minimap: {
+												enabled: false
+											},
+											readOnly: true,
+											padding: {
+												top: 30
+											},
+											language: "python",
+											automaticLayout: true,
+											fontSize: 16
+										}}
+										theme="vs-dark"
+										on:ready={(event) => console.log(event.detail)}
+										bind:value={pythonCodeSnippet} />
+								</div>
 							</div>
 						</div>
 					</div>
