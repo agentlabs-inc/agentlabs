bf628eb56aa3efbc8f524234b214d315e09b849b
Aurélien Brabant <main@aurelienbrabant.fr>
2023-10-02 13:51:54 +0200

    Add way to access http api from python sdk

---
+++ sdks/python-sdk/agentlabs/http.py
@@ -0,0 +1,30 @@
+from typing import TypedDict
+import requests
+
+from .attachment import Attachment
+
+class HttpApiConfig(TypedDict):
+    agentlabs_url: str;
+    project_id: str;
+    agent_id: str;
+
+# base url pattern
+class HttpApi:
+    def __init__(self, config: HttpApiConfig):
+        self.config = config
+        self.client = requests.Session()
+        self.client.params = {
+            "projectId": config["project_id"],
+            "agentId": config["agent_id"]
+        }
+
+    def _make_url(self, path: str):
+        return self.config["agentlabs_url"] + path
+
+    def create_message_attachment(self, message_id: str, attachment: Attachment):
+        url = self._make_url("/agent-attachments/createMessageAttachmentSync")
+        requests.post(url, params={
+            message_id: message_id
+        }, files={
+            "file": attachment.buffer
+        })
