da732f222f78b92acbd84f49610dac32d3c77bfa
Aurélien Brabant <main@aurelienbrabant.fr>
2023-10-04 21:09:51 +0200

    Add basic markdown support

---
+++ frontend/src/lib/components/markdown/markdown-link.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { urlUtils } from '@magidoc/plugin-svelte-marked'
+  import type { MarkdownOptions } from '@magidoc/plugin-svelte-marked'
+
+  export let token: any;
+  export let options: MarkdownOptions
+</script>
+
+<a
+class="underline hover:opacity-80"
+target={'_blank'}
+rel={'noopener noreferrer'}
+  href={urlUtils.isRelative(token.href)
+    ? urlUtils.joinUrlPaths(options.baseUrl, token.href)
+    : token.href}
+  title={token.title}
+>
+  <slot />
+</a>
