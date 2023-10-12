e36a072b88eefa12eef01bba7cdff3d152edf33d
Aurélien Brabant <main@aurelienbrabant.fr>
2023-10-12 20:06:05 +0200

    ✨ Make markdown code blocks copiable and display the language as well

--- frontend/src/lib/components/markdown/markdown-code.svelte
+++ frontend/src/lib/components/markdown/markdown-code.svelte
@@ -2,6 +2,7 @@
 import Highlight from 'svelte-highlight';
 import { python, shell, typescript } from 'svelte-highlight/languages';
 import atomOneDark from 'svelte-highlight/styles/atom-one-dark';
+import { Icon, ClipboardDocument } from 'svelte-hero-icons';
 
 export let token: any
 
@@ -14,10 +15,24 @@ const languages: Record<string, Highlight['language']> = {
 
 const language = languages[token.lang] || typescript
 
+const handleCopy = () => {
+	navigator.clipboard?.writeText(token.text)
+}
+
 </script>
 
 <svelte:head>
 {@html atomOneDark}
 </svelte:head>
 
-<Highlight style={atomOneDark} language={language} code={token.text} class="antialiased rounded-lg text-sm rounded-sm" />
+<div
+	class="antialiased rounded-lg text-sm rounded-md shadow-sm overflow-hidden relative"
+>
+	<div class="absolute top-0 right-0 p-2 flex items-center gap-x-2">
+		<span class="text-xs text-body-base dark:text-body-base">{token.lang}</span>
+		<button on:click={handleCopy} class="hover:opacity-80 active:opacity-50">
+			<Icon src={ClipboardDocument} class="w-5 h-5 text-body-base dark:text-body-base" />
+		</button>
+	</div>
+	<Highlight style={atomOneDark} language={language} code={token.text}  />
+</div>
