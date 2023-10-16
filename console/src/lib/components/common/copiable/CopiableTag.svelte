e274e7dffcd412c277ee1fa038e60a841385ad5d
Kevin Piacentini <kevinpiac@gmail.com>
2023-10-16 18:21:24 +0200

    🐛 Fix oauth callback url + add copiable redirect uri in console

--- console/src/lib/components/common/copiable/CopiableTag.svelte
+++ console/src/lib/components/common/copiable/CopiableTag.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { DocumentDuplicate, DocumentCheck, Icon } from "svelte-hero-icons";
 	import { onMount } from "svelte";
-
 	export let value = "";
 	export let displayedValue = "";
+	export let label = "";
 
 	let isCopied = false;
 
@@ -25,6 +25,9 @@
 <button
 	on:click={handleCopy}
 	class="bg-background-accent dark:bg-background-accent-dark flex items-center gap-2 border border-stroke-accent dark:border-stroke-accent-dark rounded-full py-1.5 px-5 text-body-base dark:text-body-base-dark text-sm antialiased cursor-pointer max-w-full">
+	{#if label?.length > 0}
+		<span>{label}</span>
+	{/if}
 	{#if !isCopied}
 		<Icon src={DocumentDuplicate} width="15" class="shrink-0" />
 	{:else}
