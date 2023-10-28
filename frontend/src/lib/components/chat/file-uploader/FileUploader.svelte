10968f8bb19577ce117ab9479d29f4b06ad59620
Kevin Piacentini <kevinpiac@gmail.com>
2023-10-28 18:39:35 +0200

    🐛 Fix attachment uploader z-index

--- frontend/src/lib/components/chat/file-uploader/FileUploader.svelte
+++ frontend/src/lib/components/chat/file-uploader/FileUploader.svelte
@@ -1,38 +1,39 @@
 <script lang="ts">
 	import { Icon, PaperClip } from "svelte-hero-icons";
 
-export let handleFile: (file: File) => void;
-export let uploadCount = 0;
+	export let handleFile: (file: File) => void;
+	export let uploadCount = 0;
 
-let fileInput: HTMLInputElement;
+	let fileInput: HTMLInputElement;
 
-const handleClick = () => {
-	fileInput.click();
-};
+	const handleClick = () => {
+		fileInput.click();
+	};
 
+	const handleFileChange = (event: any) => {
+		const file = event.target?.files[0];
 
-const handleFileChange = (event: any) => {
-	const file = event.target?.files[0];
+		if (!file) {
+			return;
+		}
 
-	if (!file) {
-		return;
-	}
-
-	handleFile(file);
-};
-
-
+		handleFile(file);
+	};
 </script>
 
-<div class="relative">
-		
-	<button type="button" on:click={handleClick} class="relative hover:opacity-80" title="Add attachment">
+<div class="relative z-10">
+	<button
+		type="button"
+		on:click={handleClick}
+		class="relative hover:opacity-80"
+		title="Add attachment">
 		{#if uploadCount > 0}
-			<div class="absolute -top-2 -left-2 flex items-center justify-center w-4 h-4 text-xs bg-white text-black rounded-full">
-					{uploadCount}
+			<div
+				class="absolute -top-2 -left-2 flex items-center justify-center w-4 h-4 text-xs bg-white text-black rounded-full">
+				{uploadCount}
 			</div>
 		{/if}
 		<Icon src={PaperClip} class="w-6 h-6 text-body-base dark:text-body-base-dark" />
-		<input on:change={handleFileChange} hidden bind:this={fileInput} type="file" />	
+		<input on:change={handleFileChange} hidden bind:this={fileInput} type="file" />
 	</button>
 </div>
